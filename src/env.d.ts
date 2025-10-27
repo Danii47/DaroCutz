4907cc24c1e16755ffe27f213c91e5b43a942b60
Danii47 <uranux1@gmail.com>
2025-10-27 23:01:31 +0100

    refact: enviroment types

---
+++ src/env.d.ts
@@ -0,0 +1,19 @@
+interface ImportMetaEnv {
+  readonly DATABASE_URL: string;
+  readonly JWT_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare namespace App {
+  interface Locals {
+    user: {
+      id: string;
+      email: string;
+      isAdmin: boolean;
+      isApproved: boolean;
+    } | null;
+  }
+}
