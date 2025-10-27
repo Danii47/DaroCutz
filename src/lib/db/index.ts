833fb0095b9c604194a69b803885deaa4d3f1843
Danii47 <uranux1@gmail.com>
2025-10-27 23:04:14 +0100

    feat: start db file

---
+++ src/lib/db/index.ts
@@ -0,0 +1,18 @@
+import { drizzle } from "drizzle-orm/postgres-js"
+import postgres from "postgres"
+import * as schema from "./schema"
+
+const connectionString = import.meta.env.DATABASE_URL
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL no está definida")
+}
+
+const client = postgres(connectionString)
+
+export const db = drizzle(client, { schema })
+
+export type User = typeof schema.users.$inferSelect
+export type NewUser = typeof schema.users.$inferInsert
+export type Appointment = typeof schema.appointments.$inferSelect
+export type NewAppointment = typeof schema.appointments.$inferInsert
