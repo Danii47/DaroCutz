4ae50c639d252a656e465ab0453511c95dbdbe38
Danii47 <uranux1@gmail.com>
2025-11-02 02:50:58 +0100

    feat: add new appointments get endpoint for admin

--- src/pages/api/appointments/index.ts
+++ src/pages/api/appointments/index.ts
@@ -1,8 +1,9 @@
 import { db } from "@/lib/db"
-import { appointments } from "@/lib/db/schema"
+import { appointments, users } from "@/lib/db/schema"
 import type { APIRoute } from "astro"
+import { and, eq, gt, gte, isNotNull } from "drizzle-orm"
 
-export const GET: APIRoute = async ({ locals }) => {
+export const GET: APIRoute = async ({ url, locals }) => {
   const currentUser = locals.user
 
   if (!currentUser) {
@@ -19,21 +20,84 @@ export const GET: APIRoute = async ({ locals }) => {
   }
 
   try {
-    const appointmentsList = await db
-      .select()
-      .from(appointments)
-      .orderBy(appointments.appointmentDate)
+    const tomorrow = new Date()
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    tomorrow.setHours(0, 0, 0, 0)
 
-    return new Response(
-      JSON.stringify({
-        success: true,
-        appointments: appointmentsList
-      }),
-      {
-        status: 200,
-        headers: { "Content-Type": "application/json" }
+    const type = url.searchParams.get("type") || "all"
+
+    if (type === "all") {
+      const appointmentsList = await db
+        .select()
+        .from(appointments)
+        .where(gt(appointments.appointmentDate, tomorrow))
+        .orderBy(appointments.appointmentDate)
+  
+      return new Response(
+        JSON.stringify({
+          success: true,
+          appointments: appointmentsList
+        }),
+        {
+          status: 200,
+          headers: { "Content-Type": "application/json" }
+        }
+      )
+    } else if (type === "reserved") {
+      if (!currentUser.isAdmin) {
+        return new Response(
+          JSON.stringify({
+            success: false,
+            error: "No tienes permisos para acceder a este recurso"
+          }),
+          {
+            status: 403,
+            headers: { "Content-Type": "application/json" }
+          }
+        )
       }
-    )
+
+      const appointmentsList = await db
+        .select({
+          id: appointments.id,
+          appointmentDate: appointments.appointmentDate,
+          status: appointments.status,
+          createdAt: appointments.createdAt,
+          userId: appointments.userId,
+          userName: users.fullName,
+          userPhone: users.phone
+        })
+        .from(appointments)
+        .leftJoin(users, eq(appointments.userId, users.id))
+        .where(and(
+          gte(appointments.appointmentDate, new Date()),
+          isNotNull(appointments.userId)
+        ))
+        .orderBy(appointments.appointmentDate)
+
+      return new Response(
+        JSON.stringify({
+          success: true,
+          appointments: appointmentsList
+        }),
+        {
+          status: 200,
+          headers: { "Content-Type": "application/json" }
+        }
+      )
+      
+    } else {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          error: "Tipo de consulta inválido"
+        }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json" }
+        }
+      )
+    }
   } catch (error) {
     console.log(error)
     return new Response(
@@ -88,6 +152,42 @@ export const POST: APIRoute = async ({ request, locals }) => {
         JSON.stringify({
           success: false,
           error: "La fecha y hora de la cita deben ser futuras."
+        }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json" }
+        }
+      )
+    }
+
+    const today = new Date();
+
+    if (timestamp.getDate() === today.getDate() &&
+      timestamp.getMonth() === today.getMonth() &&
+      timestamp.getFullYear() === today.getFullYear()) {
+
+      return new Response(
+        JSON.stringify({
+          success: false,
+          error: "La cita no puede ser para el día de hoy."
+        }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json" }
+        }
+      )
+    }
+
+    const [appointmentExists] = await db
+      .select()
+      .from(appointments)
+      .where(eq(appointments.appointmentDate, timestamp))
+
+    if (appointmentExists) {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          error: "Ya existe una cita en esa fecha y hora."
         }),
         {
           status: 400,
